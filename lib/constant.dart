3c65f2449998c22b258814865316b7540f95339e
Franck Darren <kikondanze@gmail.com>
2025-04-08 02:15:19 +0100

    ajouts

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,8 @@
-const baseURL = 'https://2e80-154-0-184-47.ngrok-free.app/api';
+const baseURL = 'https://ewuang-main-7cfdfz.laravel.cloud/api';
 const loginURL = '$baseURL/login';
 const registerURL = baseURL + '/users';
+
+// Couleurs
+const couleurPrimaire = "#183356"; //Bleu
+const couleurSecondaire = "#83CBCC"; // Turquoise
+const couleurTertiaire = "#F6E824"; // Jaune
